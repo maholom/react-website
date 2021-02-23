0397e0bb88b1406627fd97dd28fe3e5753188dde
MartaKirchgessner <martakirchgessner@gmail.com>
2021-02-23 15:58:09 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
 import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
