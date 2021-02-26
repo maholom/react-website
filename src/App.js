5af34ef1f56f07d01a6f8bb1bc0ac20fc7422c0d
MartaKirchgessner <martakirchgessner@gmail.com>
2021-02-26 20:46:22 +0100

    page transition

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import MovieDetail from './pages/MovieDetail';
 import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
 import GlobalStyle from './components/GlobalStyle';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion'; //rozeznává, kdy se renderují comp.
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          //rozpozná stránku, kde animace
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
